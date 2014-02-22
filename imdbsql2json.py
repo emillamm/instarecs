c92f88fa016dfe74c76ff487e71e60927be9ce8a
emillamm <e_millerkiller@hotmail.com>
2014-02-22 02:41:11 +0100

    Added support for retrieving images from rotten tomatoes

--- imdbsql2json.py
+++ imdbsql2json.py
@@ -9,18 +9,22 @@ import collections
 def make_json():
     engine = create_engine('mysql://emil:mrmemorex@50.17.252.200/imdb')
     connection = engine.connect()
-    q = """select title, production_year as year, m1.info as count, m2.info as rating, m1.movie_id as id
-    from title t 
+    q = """select t.title, t.production_year as year, m1.info as count, m2.info as rating, m1.movie_id as id, aka.title as title_eng
+    from title t
     inner join movie_info_idx m1 on t.id=m1.movie_id and m1.info_type_id = 100
     inner join movie_info_idx m2 on t.id=m2.movie_id and m2.info_type_id = 101
-    where t.kind_id = 1 and m1.info > 300000 and m2.info > 8 order by m2.info desc;"""
+    left join aka_title aka on t.id=aka.movie_id and aka.note like "(USA)"
+    where t.kind_id = 1 and m1.info > 150000 and m2.info > 7 order by m2.info desc limit 300;"""
 
     result = engine.execute(q)
     item_list = []
     for row in result:
         o = collections.OrderedDict()
         o['id'] = row["id"]
-        o['title'] = row["title"].decode("ISO-8859-1")
+        if not row['title_eng'] == "Null" or row['title_eng'] == None:
+            o['title'] = str(row["title_eng"]).decode("ISO-8859-1")
+        else:   
+            o['title'] = str(row["title"]).decode("ISO-8859-1")
         o['year'] = row["year"]
         o['count'] = row["count"]
         o['rating'] = row["rating"]
