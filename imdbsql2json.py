e7955badae838403bff44ff9e451a4b594c7e2b5
emillamm <e_millerkiller@hotmail.com>
2014-02-25 01:24:13 +0100

    Download images from rotten tomatoes

--- imdbsql2json.py
+++ imdbsql2json.py
@@ -9,11 +9,10 @@ import collections
 def make_json():
     engine = create_engine('mysql://emil:mrmemorex@50.17.252.200/imdb')
     connection = engine.connect()
-    q = """select t.title, t.production_year as year, m1.info as count, m2.info as rating, m1.movie_id as id, aka.title as title_eng
+    q = """select t.title, t.production_year as year, m1.info as count, m2.info as rating, m1.movie_id as id
     from title t
     inner join movie_info_idx m1 on t.id=m1.movie_id and m1.info_type_id = 100
     inner join movie_info_idx m2 on t.id=m2.movie_id and m2.info_type_id = 101
-    left join aka_title aka on t.id=aka.movie_id and aka.note like "(USA)"
     where t.kind_id = 1 and m1.info > 150000 and m2.info > 7 order by m2.info desc limit 300;"""
 
     result = engine.execute(q)
@@ -21,8 +20,11 @@ def make_json():
     for row in result:
         o = collections.OrderedDict()
         o['id'] = row["id"]
-        if not row['title_eng'] == "Null" or row['title_eng'] == None:
-            o['title'] = str(row["title_eng"]).decode("ISO-8859-1")
+        q = "select title from aka_title where movie_id = " + str(row["id"]) + " and note like \"(USA)\";"
+        result_tmp = engine.execute(q)
+        title_eng = result_tmp.fetchone()
+        if not title_eng  == None:
+            o['title'] = title_eng[0].decode("ISO-8859-1")
         else:   
             o['title'] = str(row["title"]).decode("ISO-8859-1")
         o['year'] = row["year"]
