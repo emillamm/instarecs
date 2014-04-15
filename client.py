5005104943d8c0f53e09e430ef34e2c640cb3f56
emillamm <e_millerkiller@hotmail.com>
2014-04-15 12:07:59 -0400

    Remove unused files

--- client.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import jsonrpclib
-server = jsonrpclib.Server('http://localhost:8080')
-result = server.add_user(123)
-print result
-result = server.get_rank(1)
-print result
-
